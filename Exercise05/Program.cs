b79114f15c029a5ae77d377e7e3c1250e6d31f30
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-18 13:30:32 +0300

    Задание 5. Выполнено

--- Exercise05/Program.cs
+++ Exercise05/Program.cs
@@ -3,5 +3,24 @@
 /*Mnement4813@yandex.ru*/
 /***********************/
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+Написать программу, которая принимает на вход два числа и проверяет, является ли одно из них квадратом другого.
+5, 25 -> Да
+-4, 16 -> Да
+25, 5 -> Да
+8, 9 -> Нет
+*/
+
+Console.WriteLine("Программа проверяет, является ли одно из введённых чисел квадратом другого");
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int squareNumberOne = numberOne * numberOne;
+int squareNumberTwo = numberTwo * numberTwo;
+
+if ((numberOne == squareNumberTwo) || (numberTwo == squareNumberOne))
+    Console.WriteLine("Является");
+else
+    Console.WriteLine("Не является");
