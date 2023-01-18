06621f098b192b26dbeaecbb00051a3f680d9f0b
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-18 11:28:05 +0300

    Задание 1. Выполнено

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/***********************/
+/*****Te3Ka_PaynE*******/
+/*Mnement4813@yandex.ru*/
+/***********************/
+
+/*
+Написать программу, которая выводит случайно число из отрезка [10, 99] и показывает наибольшую цифру этого числа
+78 -> 8
+12 -> 2
+85 -> 8
+*/
+
+Console.WriteLine("Программа генерирует число от 10 до 99, после чего выводит наибольшее из чисел.");
+int randomNumber = new Random().Next(10, 100);
+Console.WriteLine("Сгенерированное число = " + randomNumber);
+
+int numberTens = randomNumber / 10;
+int numberUnits = randomNumber % 10;
+
+if (numberTens > numberUnits)
+    Console.WriteLine("Наибольшая цифра в числе: " + numberTens);
+else
+    Console.WriteLine("Наибольшая цифра в числе: " + numberUnits);
