701c0e689fec1999054cba3091ae7219fa328c9d
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-18 13:04:49 +0300

    Задание 5. Начало

---
+++ Exercise05/Program.cs
@@ -0,0 +1,7 @@
+﻿/***********************/
+/*****Te3Ka_PaynE*******/
+/*Mnement4813@yandex.ru*/
+/***********************/
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
